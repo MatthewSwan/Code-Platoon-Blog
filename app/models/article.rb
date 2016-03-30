e99647b81435d18f469389cafa695242ab4b7716
Matthew Swan <swanm86@gmail.com>
2016-03-30 13:18:11 -0500

    added users controller and default user

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,8 +1,7 @@
 class Article < ActiveRecord::Base
   belongs_to :user
   has_many :comments, dependent: :destroy
-  validates :title, presence: true,
-                    length: { minimum: 5 }
+  validates :title, presence: true
 
   def summary
     text.split("\n\n").first
