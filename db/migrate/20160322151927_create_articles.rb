32c31ab074440ff7720ea99d7477264136453901
Matthew Swan <swanm86@gmail.com>
2016-04-07 08:14:41 -0500

    updated articles db so articles belong to users

--- db/migrate/20160322151927_create_articles.rb
+++ db/migrate/20160322151927_create_articles.rb
@@ -2,8 +2,8 @@ class CreateArticles < ActiveRecord::Migration
   def change
     create_table :articles do |t|
       t.string :title
-      t.text :text
-
+      t.text :text #probably make a more descriptive name
+      t.belongs_to :user
       t.timestamps null: false
     end
   end
