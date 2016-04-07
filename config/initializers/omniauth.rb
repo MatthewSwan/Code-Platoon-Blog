e26ca4699625c311ebef00affeb71ffc0f789d22
Matthew Swan <swanm86@gmail.com>
2016-04-07 14:32:17 -0500

    made github keys hidden from public view

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,4 @@
 # config/initializers/omniauth.rb
 Rails.application.config.middleware.use OmniAuth::Builder do
-   ENV['GITHUB_KEY'] = 'c9949bea1ff1e9f270ed'
-   ENV['GITHUB_SECRET'] = 'ba1bce0f7b80f829882e4fb774aac97ab5d6d3cd'
-  provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET'], scope: "user:email,user:follow"
+  provider :github, Rails.application.secrets.github_key, Rails.application.secrets.github_secret, scope: "user:email,user:follow"
 end
